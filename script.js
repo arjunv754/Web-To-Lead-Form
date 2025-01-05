db35b466c82de0c7c270a07bbd244b40870e88c1
arjun <arjunv754@gmail.com>
2025-01-05 13:43:41 +0530

    mandatory reCaptcha

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function handlesubmit(){
+let recaptchachecked = false;
+
+
+function handlesubmit(event){
+    if(recaptchachecked){
     let outputdata = document.querySelector(".outputdata");
     let inputdate = document.querySelector(".inputdata");
     let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdata.value =formatteddate ;
+    }
+    else{
+        alert('Please check the reCaptcha.');
+        event.preventDefault();
+    }
 }
 function timestamp()
  { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") 
@@ -10,3 +19,6 @@ function timestamp()
         elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } 
         setInterval(timestamp, 500); 
+function captchasuccess(){
+    recaptchachecked = true;
+}
